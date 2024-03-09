55fc627ef695bbda7c23ede61f8776ccb944bcda
miche <roxana.valencia@catolica.edu.sv>
2024-03-09 11:17:51 -0600

    Agregar archivos de proyecto.

---
+++ PARCIAL1B/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.Extensions.DependencyInjection;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+//Conexion
+builder.Services.AddDbPContex<PARCIAL1B>(OptionsBuilderConfigurationExtensions =>
+options.UseSqlServer(
+    builder.Configuration.GetConnectionString("BaseX")
+)
+);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
